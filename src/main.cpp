9001e10b991853876f055375ff5e43f933fd494e
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-02-11 12:35:01 +0530

    day 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include "raylib.h"
+#include <bits/stdc++.h>
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
     InitWindow(screenWidth, screenHeight, "raylib - basic window");
     SetTargetFPS(60);
-
+    std::string text = "";
     while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
+        int key_pressed = GetCharPressed();
+        if (key_pressed>0){
+            text = "You pressed : ";
+            text += char(key_pressed);
+            key_pressed = GetCharPressed();
+        }
+
+        int mouseX = GetMouseX();
+        int mouseY = GetMouseY();
+        std::string mousePos = "";
+
+        if (mouseX && mouseY) {
+            mousePos = "mouse is at : " + std::to_string(mouseX) + " , " + std::to_string(mouseY);
+            mouseX = GetMouseX();
+            mouseY = GetMouseY();
+        }
+
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText(text.c_str(),190,200,20,LIGHTGRAY);
+            DrawText("press any key",190,230,20,RED);
+            DrawText(mousePos.c_str(),190,250,20,DARKGRAY);
+            EndDrawing();
+        }
 
     CloseWindow();
     return 0;
