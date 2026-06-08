d43794a124979b546eda51369f1ff3c23a4eb5a2
korrykatti <143781663+KorryKatti@users.noreply.github.com>
2026-06-08 15:43:46 +0530

    added

--- src/UIHelper.h
+++ src/UIHelper.h
@@ -49,8 +49,42 @@ class UIHelper {
 
         }
         static void drawPlayerList(const std::vector<OnlinePlayer>& players, int& selectedPort, std::string& selectedIP){
-            // TODO
+            ClearBackground(DARKBLUE);
+            DrawText("ONLINE PLAYERS",GetScreenWidth()/2-150,50,40,WHITE);
+            DrawText("Click a player to challenge them",GetScreenWidth()/2-160,100,20,GRAY);
+
+            int y=160;
+            for (size_t i=0;i<players.size()&&i<10;i++){
+                Rectangle playerBtn = {GetScreenWidth()/2-200,(float)y,400,60};
+                bool hover = CheckCollisionPointRec(GetMousePosition(),playerBtn);
+                Color btnColor = hover? PURPLE:DARKPURPLE;
+                DrawRectangleRec(playerBtn,btnColor);
+                DrawRectangleLinesEx(playerBtn,2,GOLD);
+
+                // username and elo
+                std::string display = players[i].username + " (ELO: " + std::to_string(players[i].elo)+ ")";
+                DrawText(display.c_str(),playerBtn.x+20,playerBtn.y+40,15,LIGHTGRAY);
+
+                if (hover && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)){
+                    selectedIP = players[i].game_ip;
+                    selectedPort = players[i].game_port;
+                }
+                y+=80;
+            }
+            if (players.empty()){
+                DrawText("No players online",GetScreenWidth()/2-100,200,25,GRAY);
+                DrawText("Host a game to appear here",GetScreenWidth()/2-120,240,20,GRAY);
+            }
+            // back button
+            Rectangle backBtn = {50,GetScreenHeight()-80,150,50};
+            bool backHover = CheckCollisionPointRec(GetMousePosition(),backBtn);
+
+            DrawRectangleRec(backBtn,backHover?RED:MAROON);
+
+            DrawText("BACK",backBtn.x+45,backBtn.y+12,25,WHITE);
         }
+
+        // homelander happy very large sunlight emoji
 }
 
 #endif UI_HELPER_H
